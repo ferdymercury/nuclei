2662b6714ab6e650aba45a624482e7ed27ad577e
mnagl <mnagl@4e419536-2157-47c4-8b72-7732f613270d>
2012-08-03 22:47:29 +0000

    Tagging version 2.3

---
+++ tags/2.3/ENSDFDataSource.h
@@ -0,0 +1,55 @@
+#ifndef ENSDFDATASOURCE_H
+#define ENSDFDATASOURCE_H
+
+#include <QStringList>
+#include <QSharedPointer>
+#include <QMap>
+#include <QMetaType>
+#include <QVariant>
+#include <QMutex>
+
+#include "AbstractDataSource.h"
+
+class ENSDFParser;
+
+class ENSDFTreeItem : public AbstractTreeItem
+{
+public:
+    explicit ENSDFTreeItem(ItemType type = UnknownType, AbstractTreeItem *parent = 0);
+    explicit ENSDFTreeItem(ItemType type, const QList<QVariant> &data, unsigned int A, bool isdecay, AbstractTreeItem *parent = 0);
+    virtual ~ENSDFTreeItem();
+
+    friend QDataStream & operator<<(QDataStream &out, const ENSDFTreeItem &treeitem);
+    friend QDataStream & operator>>(QDataStream &in, ENSDFTreeItem &treeitem);
+};
+
+Q_DECLARE_METATYPE(ENSDFTreeItem)
+
+class ENSDFDataSource : public AbstractDataSource
+{
+public:
+    explicit ENSDFDataSource(QObject *parent = 0);
+    virtual ~ENSDFDataSource();
+
+    virtual AbstractTreeItem * rootItem() const;
+
+    virtual QSharedPointer<Decay> decay(const AbstractTreeItem *item) const;
+
+private:
+    QList<unsigned int> getAvailableDataFileNumbers() const;
+
+    QString cachePath;
+
+    static const quint32 magicNumber;
+    static const quint32 cacheVersion;
+
+    bool loadENSDFCache();
+    void createENSDFCache();
+
+    ENSDFTreeItem *root;
+
+    mutable ENSDFParser *mccache;
+    mutable QMutex m;
+};
+
+#endif // ENSDFDATASOURCE_H
---
+++ tags/2.3/version.h
@@ -0,0 +1,41 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION "2.3"
+
+// used to save windows state. must be incremented if window layout changes!
+#define UIVERSION 0
+
+#define NUCLEIURL "http://nuclei.sf.net"
+
+#define NUCLEIABOUT "\
+<p><b>Nuclei</b></p><p>shows decay schemes, computes anisotropies of decay cascades and \
+shows the expected photo peak spectra.</p> \
+\
+<p>Copyright 2012 Matthias Nagl, Georg-August Universität Göttingen</p> \
+<p><a href=\"" NUCLEIURL "\">" NUCLEIURL "</a></p> \
+"
+
+#define LIBAKKABOUT "\
+<p>Nuclei uses <b>libakk</b> for anisotropy calculations</p> \
+<p>Copyright 2012 João Guilherme Martins Correia & Marcelo Baptista Barbosa, CERN</p> \
+"
+
+#define GPL "\
+<p>Nuclei as well as libAkk is free software: you can redistribute it and/or modify \
+it under the terms of the GNU General Public License as published by \
+the Free Software Foundation, either version 3 of the License, or \
+(at your option) any later version.</p> \
+ \
+<p>This program is distributed in the hope that it will be useful, \
+but WITHOUT ANY WARRANTY; without even the implied warranty of \
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the \
+GNU General Public License for more details.</p> \
+ \
+<p>You should have received a copy of the GNU General Public License \
+along with this program.  If not, see <a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>.</p> \
+"
+
+#define NUCLEIVERSIONURL "http://nuclei.sourceforge.net/versioninfo/versions.txt"
+
+#endif // VERSION_H
